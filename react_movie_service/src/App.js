8c1bf3d84a10e52e3081727e9c326cee347d15a0
iheehee <iheeheedev@gmail.com>
2022-07-18 16:19:46 +0900

    add router

--- react_movie_service/src/App.js
+++ react_movie_service/src/App.js
@@ -1,30 +1,16 @@
-import Button from "./Button";
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState("true");
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => { 
-      setCoins(json); 
-      setLoading(false);
-    });
-  }, [] );
   return (
-    <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
